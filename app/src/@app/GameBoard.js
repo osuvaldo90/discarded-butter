5e05e245cdbfdb5779b1d777b06426419cd344ac
Osuvaldo Ramos <osuvaldo@divvyhomes.com>
2020-03-29 22:03:44 -0700

    Add count down to progress bar

--- app/src/@app/GameBoard.js
+++ app/src/@app/GameBoard.js
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types'
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Container, Row, Col, ProgressBar } from 'react-bootstrap'
 
 import GameCard from './GameCard'
@@ -15,32 +15,61 @@ const CARDS_IN_HAND = [
   { id: 6, text: 'Proin lobortis' },
 ]
 
+const TIME_LIMIT = 60000
+
 const GameBoard = ({ players }) => {
   const [selectedCardIndex, setSelectedCardIndex] = useState()
+  const [startTime] = useState(Date.now())
+  const [msLeft, setMsLeft] = useState(TIME_LIMIT)
+
+  const secondsLeft = Math.floor(msLeft / 1000)
+
+  useEffect(() => {
+    if (msLeft > 0) {
+      setTimeout(() => {
+        const elapsed = Date.now() - startTime
+        setMsLeft(Math.max(0, TIME_LIMIT - elapsed))
+      }, 100)
+    }
+  }, [startTime, msLeft])
+
+  const warningThreshold = TIME_LIMIT / 3
+  const dangerThreshold = TIME_LIMIT / 6
+
+  const progressBarVariant =
+    msLeft > warningThreshold ? 'primary' : msLeft > dangerThreshold ? 'warning' : 'danger'
 
   return (
     <Container bg="light">
       <Row className="mt-4 h-100">
         <Col>
-          <Row className="sticky-top bg-white border-bottom">
-            <Col>
-              <GameCard className="mb-2" variant="dark">
+          <Row xs={1} md={2}>
+            <Col className="mb-3 sticky-top">
+              <GameCard className="h-100" variant="dark">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eget pulvinar
                 lacus
-                <ProgressBar className="mt-3" min={0} max={60} now={47} animated />
+                {secondsLeft > 0 ? (
+                  <ProgressBar
+                    className="mt-3"
+                    variant={progressBarVariant}
+                    min={0}
+                    max={TIME_LIMIT / 1000}
+                    now={secondsLeft}
+                    label={`${secondsLeft}s`}
+                    animated
+                  />
+                ) : (
+                  <div className="mt-2 text-white-50">Time&apos;s up!</div>
+                )}
               </GameCard>
             </Col>
-          </Row>
-          <Row className="d-md-none">
-            <Col className="mt-3">
+            <Col className="mb-3 d-md-none">
               <PlayerList players={players} opened={false} />
             </Col>
-          </Row>
-          <Row xs={1} md={2}>
             {CARDS_IN_HAND.map(({ id, text }, index) => {
               const variant = index === selectedCardIndex ? 'selected' : 'light'
               return (
-                <Col className="mt-3" key={id}>
+                <Col key={id} className="mb-3">
                   <GameCard
                     className="h-100"
                     variant={variant}
